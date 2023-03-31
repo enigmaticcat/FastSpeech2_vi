b94b86a35e5c4d80a9542c848648a3d291edcf9b
Son Pham (PC) <sp@outlook.com.vn>
2023-03-31 20:32:02 +0700

    vn symbol

--- text/symbols.py
+++ text/symbols.py
@@ -10,12 +10,14 @@ from text import cmudict, pinyin
 _pad = "_"
 _punctuation = "!'(),.:;? "
 _special = "-"
-_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+# _letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+_letters  = '0123456789aáảàãạâấẩầẫậăắẳằẵặbcdđeéẻèẽẹêếểềễệfghiíỉìĩịjklmnoóỏòõọôốổồỗộơớởờỡợpqrstuúủùũụưứửừữựvwxyýỷỳỹỵz'
+
 _silences = ["@sp", "@spn", "@sil"]
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ["@" + s for s in cmudict.valid_symbols]
-_pinyin = ["@" + s for s in pinyin.valid_symbols]
+# _pinyin = ["@" + s for s in pinyin.valid_symbols]
 
 # Export all symbols:
 symbols = (
@@ -24,6 +26,6 @@ symbols = (
     + list(_punctuation)
     + list(_letters)
     + _arpabet
-    + _pinyin
+    #+ _pinyin
     + _silences
 )
