b7931fd4e16bd393a21a73ae8a660a3122a383b4
Son Pham (PC) <sp@outlook.com.vn>
2023-04-07 20:10:05 +0700

    fixed lexicon

--- scripts/infore_ipa/i05.bat
+++ scripts/infore_ipa/i05.bat
@@ -1,1 +1,1 @@
-python train.py --restore_step 28000 -p config/infore_ipa/preprocess.yaml -m config/infore_ipa/model.yaml -t config/infore_ipa/train.yaml
+python train.py -p config/infore_ipa/preprocess.yaml -m config/infore_ipa/model.yaml -t config/infore_ipa/train.yaml
