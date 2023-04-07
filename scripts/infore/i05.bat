84365c033de19804a252a8eb9245edbf592459fc
Son Pham (PC) <sp@outlook.com.vn>
2023-04-07 21:43:25 +0700

    script 60k

--- scripts/infore/i05.bat
+++ scripts/infore/i05.bat
@@ -1,1 +1,1 @@
-python train.py --restore_step 28000 -p config/infore/preprocess.yaml -m config/infore/model.yaml -t config/infore/train.yaml
+python train.py --restore_step 60000 -p config/infore/preprocess.yaml -m config/infore/model.yaml -t config/infore/train.yaml
